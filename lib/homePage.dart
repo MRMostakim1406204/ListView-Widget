00c3415f9c03542d57e6ddde879dbb58a95c143a
MRMostakim1406204 <mdmrmostakim1406204@gmail.com>
2023-10-14 12:39:47 +0600

    listView-Widget

---
+++ lib/homePage.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height/8,
+            width: MediaQuery.of(context).size.width/8,
+           color: Colors.deepOrange,
+          ),
+          Divider(),
+          Container(
+            height: MediaQuery.of(context).size.height/8,
+            width: MediaQuery.of(context).size.width/8,
+           color: Colors.black,
+          ),
+          Divider(),
+          Container(
+            height: MediaQuery.of(context).size.height/8,
+            width: MediaQuery.of(context).size.width/8,
+           color: Colors.pink,
+          ),
+          Divider(),
+          Container(
+            height: MediaQuery.of(context).size.height/8,
+            width: MediaQuery.of(context).size.width/8,
+           color: Colors.blue,
+          ),
+          Divider(),
+          Container(
+            height: MediaQuery.of(context).size.height/8,
+            width: MediaQuery.of(context).size.width/8,
+           color: Colors.green,
+          ),
+          Divider(),
+          Container(
+            height: MediaQuery.of(context).size.height/8,
+            width: MediaQuery.of(context).size.width/8,
+           color: Colors.red,
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height/8,
+            width: MediaQuery.of(context).size.width/8,
+           color: Colors.yellow,
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height/8,
+            width: MediaQuery.of(context).size.width/8,
+           color: const Color.fromARGB(255, 79, 244, 54),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height/8,
+            width: MediaQuery.of(context).size.width/8,
+           color: Colors.grey,
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height/8,
+            width: MediaQuery.of(context).size.width/8,
+           color: Color.fromARGB(240, 146, 25, 150),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:list_view_widget/listView.builder.dart';
+ import 'package:flutter/material.dart';
+import 'package:list_view_widget/homePage.dart';
 
 void main() => runApp(MyApp());
 
